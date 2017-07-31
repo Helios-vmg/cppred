26dc608717b4f8648a441b2ef409cde21271511a
Víctor M. González <helios.vmg@gmail.com>
2017-07-31 02:27:32 -0300

    Added asm->text converter.

---
+++ process_text/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace process_text
+{
+    static class Utility
+    {
+        public static string[] ToStrings(this Match match)
+        {
+            var list = new List<string>();
+            foreach (var g in match.Groups)
+                list.Add(g.ToString());
+            return list.Skip(1).ToArray();
+        }
+    }
+}
