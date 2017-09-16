55b30261ddffa0b1c7cab108a78bbe7841d834e6
Víctor M. González <helios.vmg@gmail.com>
2017-09-16 01:42:23 -0300

    Deleted old projects.

--- code_generator/CpuDefinition.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class CodeGenerator;
-
-class CpuDefinition{
-public:
-	void generate(unsigned opcode, CodeGenerator &);
-	void generate(unsigned first_opcode, unsigned second_opcode, CodeGenerator &);
-};
--- code_generator/FlagSetting.cpp
+++
@@ -1,6 +0,0 @@
-#include "FlagSetting.h"
-
-FlagSetting FlagSetting::Reset(Operation::Reset);
-FlagSetting FlagSetting::Set(Operation::Set);
-FlagSetting FlagSetting::Keep(Operation::Keep);
-FlagSetting FlagSetting::Flip(Operation::Flip);
