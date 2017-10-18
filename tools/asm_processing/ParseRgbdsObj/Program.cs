5a39dd4e78376f5740f052417169a69c15ead1aa
Víctor M. González <helios.vmg@gmail.com>
2017-10-18 02:33:36 -0300

    Fixed extraction of wave pointer data.

--- tools/asm_processing/ParseRgbdsObj/Program.cs
+++ tools/asm_processing/ParseRgbdsObj/Program.cs
@@ -11,16 +11,42 @@ namespace ParseRgbdsObj
     {
         static void Main(string[] args)
         {
-            var parser = new ObjParser(@"audio_red.o");
-            //foreach (var symbol in parser.Symbols)
-            //{
-            //    Console.WriteLine(symbol.Name);
-            //}
-
-            for (int i = 6; i <= 8; i++)
+            try
             {
-                var section = parser.Sections[i];
-                Console.WriteLine(Convert.ToBase64String(section.Data.Skip(198).Take(16).ToArray()));
+                var parser = new ObjParser(@"audio_red.o");
+                //foreach (var symbol in parser.Symbols)
+                //{
+                //    Console.WriteLine(symbol.Name);
+                //}
+
+                var symbols = new[]
+                {
+                    "Audio1_WavePointers",
+                    "Audio2_WavePointers",
+                    "Audio3_WavePointers",
+                };
+
+                {
+                    var symbol = parser.Symbols.Find(s => s.Name == symbols[0]);
+                    var section = parser.Sections[(int)symbol.SectionId];
+                    for (int i = 0; i < 5; i++)
+                    {
+                        var data = section.Data.Skip((int) symbol.SectionOffset + 9*2 + 16*i).Take(16).ToArray();
+                        Console.WriteLine($"{{ 0x{BitConverter.ToString(data).ToUpper().Replace("-", ", 0x")}, }},");
+                    }
+                }
+
+                foreach (var symbolName in symbols)
+                {
+                    var symbol = parser.Symbols.Find(s => s.Name == symbolName);
+                    var section = parser.Sections[(int) symbol.SectionId];
+                    var data = section.Data.Skip((int)symbol.SectionOffset + 9 * 2 + 16 * 5).Take(16).ToArray();
+                    Console.WriteLine($"{{ 0x{BitConverter.ToString(data).ToUpper().Replace("-", ", 0x")}, }},");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
             }
         }
     }
