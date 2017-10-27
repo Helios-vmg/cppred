98ead0b45817ad6a98773bc373d29ca72905fa52
Víctor M. González <helios.vmg@gmail.com>
2017-10-27 07:26:58 -0300

    Bundled FreeImage is now built on UNIX, instead of requiring a system one.

--- build_unix.sh
+++ build_unix.sh
@@ -2,11 +2,24 @@
 
 cpu_count=$(nproc)
 
+# Build FreeImage
+cd FreeImage
+make -j $cpu_count
+cd ..
+
+# Build code generator
 cd code_generation
 cmake .
 make -j $cpu_count
-cd ../CodeGeneration
+cd ..
+
+# Generate static resources
+cd CodeGeneration
 ../code_generation/code_generation
-cd ../cppred
+cd ..
+
+# Build game
+cd cppred
 cmake .
 make -j $cpu_count
+cd ..
